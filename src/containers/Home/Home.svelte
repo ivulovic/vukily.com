d5d59084b1d10ee366f0a9bf4cf3ba02e2a69890
ivulovic <ivn.vulovic@gmail.com>
2020-11-09 00:13:47 +0100

    simplified home page

--- src/containers/Home/Home.svelte
+++ src/containers/Home/Home.svelte
@@ -61,8 +61,8 @@
   }
   .col-3 {
     display: grid;
-    /* grid-template-columns: 1fr 1fr 1fr; */
-    grid-template-columns: 1fr 2fr;
+    grid-template-columns: 1fr 1fr 1fr;
+    /* grid-template-columns: 1fr 2fr; */
     grid-column-gap: 2rem;
     grid-row-gap: 2rem;
     margin: 1.2rem 0px;
@@ -119,18 +119,18 @@
 <Meta {metadata} />
 <main>
   <h1 class="title">IVAN VULOVIĆ</h1>
-  <h2 class="subtitle">
+  <!-- <h2 class="subtitle">
     Bachelor of Science in Informatics &middot; JavaScript Developer &middot;
     Belgrade, Serbia
-  </h2>
-  <h3 class="about">
+  </h2> -->
+  <!-- <h3 class="about">
     Ovaj sajt postoji jer sam želeo da pokažem svoj način razmišljanja i kako
-    koristim tehnologiju. Najveći deo ćete videti u sekciji <a
-      href="/blog">Blog</a> . Tu će biti dosta razmišljanja i komentara na razne
-    teme iz sveta i generalno dešavanja na tehnološkoj sceni. Ovde ću predstaviti
-    i projekte na kojima radim, kao i one koje sam napustio, koji su razlozi za sve
-    to, kao i koje jezike koristim dok razvijam aplikacije.
-  </h3>
+    koristim tehnologiju. Najveći deo ćete videti u sekciji
+    <a href="/blog">Blog</a>. Tu će biti dosta razmišljanja i komentara na razne
+    teme iz sveta i generalno dešavanja na tehnološkoj sceni. Ovde ću
+    predstaviti i projekte na kojima radim, kao i one koje sam napustio, koji su
+    razlozi za sve to, kao i koje jezike koristim dok razvijam aplikacije.
+  </h3> -->
   <div class="col-3">
     <div>
       <h2 class="subtitle uppercase">
@@ -165,72 +165,35 @@
       <ul>
         <li>
           <a href="https://spisko.com" target="new" tinro-ignore>
-            <p class="link-title">
-              2020 &middot; Spisko - Vaš lični spisak, Vaša lista želja
-            </p>
-            <small class="accent">
-              Projekat je u izradi, ali je izbačena inicijalna funkcionalna
-              verzija.
-            </small>
-            <p class="link-description">
-              Jedinstvena platforma liste želja koja objedinjuje više različitih
-              prodavnica. Ideja projekta je da korisnici mogu da postave svoju
-              želju samo kopiranjem linka u aplikaciju, tako da onda svi njihovi
-              prijatelji mogu da vide i ispune. Savršena je za rođendane i druge
-              prilike, gde svojim prijateljima treba da kupite poklon, a da to
-              ne bude nešto što njima nije potrebno. Korišćene tehnologije su
-              React + Redux na frontendu, a na bekendu NodeJS uz MongoDB bazu.
-            </p>
+            <p class="link-title">2020 &middot; Spisko</p>
+            <p class="link-description">Tvoj lični spisak želja</p>
           </a>
         </li>
       </ul>
-      <strong class="subtitle subsubtitle uppercase">
-        Iz studentskih dana
-      </strong>
+      <!-- <strong class="subtitle subsubtitle uppercase"> ... </strong> -->
+    </div>
+    <div>
+      <h2 class="subtitle uppercase">Kontakt</h2>
       <ul>
         <li>
           <a
-            href="http://studentarija.herokuapp.com/"
+            href="https://www.linkedin.com/in/ivulovic/"
             target="new"
             tinro-ignore>
-            <p class="link-title">2018 &middot; Studentarija</p>
-            <p class="link-description">
-              Studentarija je portal za studente, koji se bavi aktuelnim
-              studentskim temama. Postoje sve bazične funkcionalnosti jednog
-              portala: od logovanja, do pravljenja, uređivanja i brisanja
-              postova, pa naravno i uređivanja komentara, upravljanjem žalbama
-              na korisnike i banovanjem istih. Pravljen je za potrebe završnog
-              rada, a korišćene tehnologije su Sass, Angular + NgRx, uz NodeJS
-              na bekendu, uz MongoDB bazu.
-            </p>
+            <p class="link-title">LinkedIn</p>
+            <p class="link-description">Povežimo se</p>
           </a>
         </li>
         <li>
-          <a
-            href="https://ivanreacts-9ae4b.firebaseapp.com/"
-            target="new"
-            tinro-ignore>
-            <p class="link-title">2017 &middot; Koktel Bar Crystal</p>
-            <p class="link-description">
-              Single page aplikacija koja predstavlja prezentaciju koktel bara.
-              Urađeno koristeći Sass i React dok je nivo znanja ovih framework-a
-              bio na početnom nivou.
-            </p>
+          <a href="https://twitter.com/vukily" target="new" tinro-ignore>
+            <p class="link-title">Twitter</p>
+            <p class="link-description">Prozborimo koju</p>
           </a>
         </li>
         <li>
-          <a
-            href="http://www.mobilestore.epizy.com/Home"
-            target="new"
-            tinro-ignore>
-            <p class="link-title">2016 &middot; Prodavnica telefona</p>
-            <div class="link-description">
-              Online prodavnica mobilnih telefona, različitih brendova, sa svim
-              njihovim specifikacijama, cenama i korpom za kupovinu. Odrađeno
-              koristeći HTML5, CSS3, Vanilla JS, jQuery, AJAX na frontendu, dok
-              je na bekendu korišćen PHP-ov framework CodeIgniter, a kao baza
-              MySQL.
-            </div>
+          <a href="https://github.com/ivulovic" target="new" tinro-ignore>
+            <p class="link-title">GitHub</p>
+            <p class="link-description">Udari gde najviše boli</p>
           </a>
         </li>
       </ul>
